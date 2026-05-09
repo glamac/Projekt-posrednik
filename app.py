73f5ca5eb4478f406f07f0cfac12a0783f58f1be
Jarimorajro <183761369+Jarimorajro@users.noreply.github.com>
2026-05-09 21:56:19 +0200

    Naprawa problem deregenracji. Wciąż brakuje blokad i priorytetów.

--- app.py
+++ app.py
@@ -43,48 +43,61 @@ def north_west_corner(supply, demand):
     return allocation
 
 
-def compute_potentials(z, allocation, n_supply, n_demand):
+def compute_potentials(z, base_ones, n_supply, n_demand):
     """Wyznacz α i β z równań: z_ij - α_i - β_j = 0 dla tras bazowych"""
     alpha = [None] * n_supply
     beta = [None] * n_demand
-    alpha[0] = 0
-    changed = True
 
-    while changed:
+    alpha[0] = 0
+    
+    while None in alpha or None in beta:
         changed = False
         for i in range(n_supply):
             for j in range(n_demand):
-                if allocation[i][j] > 0:
+                if base_ones[i, j]:
                     if alpha[i] is not None and beta[j] is None:
-                        beta[j] = z[i][j] - alpha[i]
+                        beta[j] = z[i, j] - alpha[i]
                         changed = True
-                    if beta[j] is not None and alpha[i] is None:
-                        alpha[i] = z[i][j] - beta[j]
+                    elif beta[j] is not None and alpha[i] is None:
+                        alpha[i] = z[i, j] - beta[j]
                         changed = True
-    return alpha, beta
+
+        if not changed and (None in alpha or None in beta):
+            found = False
+            for i in range(n_supply):
+                for j in range(n_demand):
+                    if not base_ones[i, j]:
+                        if (alpha[i] is not None and beta[j] is None) or \
+                           (beta[j] is not None and alpha[i] is None):
+                            base_ones[i, j] = True
+                            found = True
+                            break
+                if found: break
+
+    return alpha, beta, base_ones
 
 
-def compute_deltas(z, alpha, beta, allocation, n_supply, n_demand):
+def compute_deltas(z, alpha, beta, base_ones, n_supply, n_demand):
     """Δ_ij = z_ij - α_i - β_j dla komórek niebazowych"""
     deltas = np.full((n_supply, n_demand), -np.inf)
     for i in range(n_supply):
         for j in range(n_demand):
-            if allocation[i][j] == 0 and alpha[i] is not None and beta[j] is not None:
+            if not base_ones[i][j] and alpha[i] is not None and beta[j] is not None:
                 deltas[i][j] = z[i][j] - alpha[i] - beta[j]
     return deltas
 
 
-def find_cycle(allocation, start_i, start_j, n_supply, n_demand):
+def find_cycle(base_ones, start_i, start_j, n_supply, n_demand):
     """Znajdź cykl korekcyjny dla danej komórki"""
     for i in range(n_supply):
-        if i != start_i and allocation[i][start_j] > 0:
+        if i != start_i and base_ones[i][start_j]:
             for j in range(n_demand):
-                if j != start_j and allocation[start_i][j] > 0 and allocation[i][j] > 0:
+                if j != start_j and base_ones[start_i][j] and base_ones[i][j]:
                     return [(start_i, start_j), (i, start_j), (i, j), (start_i, j)]
     return None
 
 
-def improve_solution(allocation, cycle):
+def improve_solution(allocation, base_ones, cycle):
     """Poprawa rozwiązania – przesunięcie w cyklu (+ - + -)"""
     if not cycle:
         return allocation
@@ -96,19 +109,30 @@ def improve_solution(allocation, cycle):
             allocation[i][j] += min_val
         else:
             allocation[i][j] -= min_val
-    return allocation
+    
+    base_ones[cycle[0][0]][cycle[0][1]] = True
+    removed = False
+    for (i, j) in cycle[1::2]:
+        # Musimy usunąć dokładnie jedną komórkę, która stała się zerem
+        if allocation[i][j] == 0:
+            base_ones[i][j] = False
+            removed = True
+            break
+
+    return allocation, base_ones
 
 
 def solve_intermediary(z, supply, demand, max_iter=100):
     """Rozwiązuje zagadnienie pośrednika (maksymalizacja)"""
     n_supply, n_demand = z.shape
     allocation = north_west_corner(supply, demand)
+    base_ones = (allocation > 0)
     history = [allocation.copy()]
     iterations_deltas = []
 
     for it in range(max_iter):
-        alpha, beta = compute_potentials(z, allocation, n_supply, n_demand)
-        deltas = compute_deltas(z, alpha, beta, allocation, n_supply, n_demand)
+        alpha, beta, base_ones = compute_potentials(z, base_ones, n_supply, n_demand)
+        deltas = compute_deltas(z, alpha, beta, base_ones, n_supply, n_demand)
 
         max_delta = np.max(deltas)
         iterations_deltas.append((it, deltas.copy(), max_delta))
@@ -119,9 +143,9 @@ def solve_intermediary(z, supply, demand, max_iter=100):
         pos = np.argwhere(deltas == max_delta)[0]
         i0, j0 = pos[0], pos[1]
 
-        cycle = find_cycle(allocation, i0, j0, n_supply, n_demand)
+        cycle = find_cycle(base_ones, i0, j0, n_supply, n_demand)
         if cycle:
-            allocation = improve_solution(allocation, cycle)
+            allocation, base_ones = improve_solution(allocation, base_ones, cycle)
             history.append(allocation.copy())
 
     total_profit = np.sum(allocation * z)
